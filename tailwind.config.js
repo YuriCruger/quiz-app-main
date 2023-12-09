ba31863e0f44e27b58e4ac1edaf384318b811069
Yuri Cruger <134614392+YuriCruger@users.noreply.github.com>
2023-12-09 15:49:03 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,8 +84,8 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       backgroundImage: {
-        patternBg: "url(./public/images/pattern-background-mobile-light.svg)",
-        patternBgDark: "url(./public/images/pattern-background-mobile-dark.svg)",
+        patternBg: "url(./images/pattern-background-mobile-light.svg)",
+        patternBgDark: "url(./images/pattern-background-mobile-dark.svg)",
       },
     },
   },
